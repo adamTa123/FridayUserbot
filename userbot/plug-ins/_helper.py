b231ed38120ee51400a9e6f0b97b15736b4778ea
adamTa123 <70377410+adamTa123@users.noreply.github.com>
2020-09-21 18:27:05 +0200

    Update and rename userbot/plugins/_helper.py to userbot/plug-ins/_helper.py

--- userbot/plug-ins/_helper.py
+++ userbot/plug-ins/_helper.py
@@ -39,7 +39,9 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Modules For\n [FridayUserBot](https://t.me/FridayOT)\n`Userbot Helper to reveal all the modules`"""
+            help_string = """Friday Userbot Modules Are Listed Here !\n
+For More Help or Support Visit @FridayOT"""
+
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
